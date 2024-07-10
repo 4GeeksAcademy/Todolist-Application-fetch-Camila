fa71c9776a69855017a3c9581545dfb9501a697c
felixcamila <138735721+felixcamila@users.noreply.github.com>
2024-07-10 18:33:09 +0000

    modificaciones en proceso PUT

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -3,72 +3,174 @@ import React, { useEffect, useState } from "react";
 function TodoList() {
     const [inputValue, setInputValue] = useState("");
     const [tareas, setTareas] = useState([]);
+    const [editLabel, setEditLabel] = useState([]);
+    const [newTarea, setNewTarea] = useState("");
 
 
-    function createUsuario() {
-        fetch('https://playground.4geeks.com/todo/users/Camila', {
+    function createUser() {
+        fetch('https://playground.4geeks.com/todo/users/Camila1234', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             }
+
         })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-        })
-        .catch(error => {
-            console.log(error);
-        });
+            .then((response) => response.json())
+
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
-    
-    function agregarTarea() {
-        fetch('https://playground.4geeks.com/todo/users/Camila', {
+
+    function obtenerTareas() {
+        fetch('https://playground.4geeks.com/todo/users/Camila1234', {
             method: "GET",
             headers: {
                 "Content-Type": "application/json"
             }
-        })
-        .then(response => response.json())
-        .then(data => {
-          setTareas(data.todos)
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }
-    
 
+        })
+            .then((response) => {
+                if (response.status === 404) {
+                    createUser()
+                    return false;
+                }
+                return response.json()
+            })
+
+
+            .then(data => {
+                if (data) {
+                    setTareas(data.todos)
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+    //modificar tareas
+    const modificarTareas = (e) => {
+        e.preventDefault();
+        if (newTarea.trim() !== "") {
+            console.log(editLabel);
+            fetch(`https://playground.4geeks.com/todo/todos/${editLabel}`, {
+                
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "label": newTarea,
+                    "is_done": true
+                })
+            })
+                .then((response) => {
+                    if (response.status === 200) {
+                        return response.json()
+                    }
+                    return false;
+                })
+                .then(data => {
+                    if (data) {
+                        const editLabel = tareas.filter(item => item.id.label !== id);
+                        setEditLabel(editLabel);
+                    }
+
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        };
+    }
+
+    //Editar tarea
+
+    const handleEdit = (id) => {
+        setEditLabel(id)
+    }
+
+    //input de modificar
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
+
+    };
+    const handleInputNewTareaChange = (e) => {
+        setNewTarea(e.target.value)
     };
 
+
+    //agregar tareas 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        if (inputValue.trim() !== "") {
-            const nuevaTarea = {
-                id: tareas.length + 1,
-                texto: inputValue,
-                completada: false
-            };
 
-            setTareas([...tareas, nuevaTarea]);
-            setInputValue("");
-        }
+        e.preventDefault();
+
+        if (inputValue.trim() !== "") {
+
+            fetch('https://playground.4geeks.com/todo/todos/Camila1234', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "label": inputValue,
+                    "is_done": false
+                })
+
+            })
+                .then((response) => {
+                    if (response.status === 201) {
+                        return response.json()
+                    }
+                    return false;
+                })
+
+
+                .then(data => {
+                    if (data) {
+                        setTareas(tareas.concat(data))
+                        setInputValue("");
+                    }
+
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        };
+
     };
+
 
     const handleDelete = (id) => {
-        const nuevasTareas = tareas.filter(item => item.id !== id);
-        setTareas(nuevasTareas);
+        fetch('https://playground.4geeks.com/todo/todos/' + id, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then((response) => {
+                if (response.status === 204) {
+                    return response
+                }
+                return false;
+            })
+
+            .then(data => {
+
+                if (data) {
+                    const nuevasTareas = tareas.filter(item => item.id !== id);
+                    setTareas(nuevasTareas);
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
     };
-
     useEffect(() => {
-        createUsuario()
-        agregarTarea()
-
+        obtenerTareas()
     }, [])
-
-
-    
 
     return (
 
@@ -86,19 +188,46 @@ function TodoList() {
             </form>
 
             <ul className="list-group mt-5">
-                {tareas.map(item=> (
+
+                {tareas.map(item => (
                     <li className="list-group-item d-flex justify-content-between align-items-start" key={item.id}>
                         <div>
                             <span className="" >{item.texto}</span>
-                             <span className="" >{item.label}</span>
-                           
+                            <span className="" >{item.label}</span>
+
                         </div>
                         <button className="btn-close" aria-label="Close" onClick={() => handleDelete(item.id)}></button>
+                        <button type="button" onClick={() => handleEdit(item.id)} className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Editar
+                        </button>
+
 
                     </li>
                 ))}
             </ul>
+
+
+
+
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={handleSubmit}> <input className="input-group w-50 " type="text" placeholder="Modificar tarea" onChange={handleInputNewTareaChange} value={newTarea} /></form>
+
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button className="btn btn-primary" onClick={(e) => modificarTareas(e)}>Modificar</button>
+                    </div>
+                </div>
+            </div>
         </div>
+        </div >
     );
 }
 
